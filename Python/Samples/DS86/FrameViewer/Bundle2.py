07c0c93556aa5c413fe7cae30ed8f074e7c07ad7
Rui Oliveira <ruijoliveira2003@gmail.com>
2026-02-10 16:49:10 +0000

    change

--- Python/Samples/DS86/FrameViewer/Bundle2.py
+++ Python/Samples/DS86/FrameViewer/Bundle2.py
@@ -82,8 +82,8 @@ def bundle(colorFrame, colorToDepthFrame, depthFrame, objects_info, colorSlope, 
     object_outOfLine = []
     belongs_to_previous = False
 
-    #colorToDepth_copy = colorFrame.copy()
-    colorToDepth_copy = colorToDepthFrame.copy()
+    colorToDepth_copy = colorFrame.copy()
+    #colorToDepth_copy = colorToDepthFrame.copy()
 
     if len(objects_info) != 0:
         for obj in objects_info:
@@ -168,20 +168,22 @@ def bundle(colorFrame, colorToDepthFrame, depthFrame, objects_info, colorSlope, 
             print("-------------------------------------------------------------------")
 
     #colorToDepth_copy = cv2.resize(colorToDepth_copy, (640, 480))
-    #Sx = numpy.tan(numpy.radians(70/2)) / numpy.tan(numpy.radians(60/2))
-    #Sy = numpy.tan(numpy.radians(50/2)) / numpy.tan(numpy.radians(45/2))
+    Sx = ((1600/2) / numpy.tan(numpy.radians(70/2))) / ((640/2) / numpy.tan(numpy.radians(60/2)))
+    Sy = ((1200/2) / numpy.tan(numpy.radians(50/2))) / ((480/2) / numpy.tan(numpy.radians(45/2)))
+    print("Sx:", Sx)
+    print("Sy:", Sy)
 
     for obj_id, contour_list in enumerate(contours, start=1):
         for c in contour_list:
             rect = cv2.minAreaRect(c)
             box = cv2.boxPoints(rect)
-            #box_scaled = numpy.copy(box)
-            #box_scaled[:,0] = (box[:,0] - cx) * Sx
-            #box_scaled[:,1] = (box[:,1] - cy) * Sy
-            #box = numpy.round(box_scaled).astype(numpy.int32)
-            box = numpy.round(box).astype(numpy.int32)
-            #cv2.drawContours(colorToDepth_copy, [box + [800, 608]], 0, (0, 255, 0), 2)
-            cv2.drawContours(colorToDepth_copy, [box], 0, (0, 255, 0), 2)
+            box_scaled = numpy.copy(box)
+            box_scaled[:,0] = (box[:,0] - cx) * Sx + 770.5248597323662 #+ 35
+            box_scaled[:,1] = (box[:,1] - cy) * Sy + 608.3483690797439 #+ 10
+            box = numpy.round(box_scaled).astype(numpy.int32)
+            #box = numpy.round(box).astype(numpy.int32)
+            cv2.drawContours(colorToDepth_copy, [box + [int(abs((770.5248597323662 + 35) - cx*2.5)), int(abs((608.3483690797439 + 10) - cy*2.5))]], 0, (0, 255, 0), 2)
+            #cv2.drawContours(colorToDepth_copy, [box], 0, (0, 255, 0), 2)
             idx_y = numpy.argmin(box[:,1])
             idx_x = numpy.argmin(box[:,0])
             x, y = box[idx_x]
@@ -194,9 +196,10 @@ def bundle(colorFrame, colorToDepthFrame, depthFrame, objects_info, colorSlope, 
                 print("Afast")
                 cv2.putText(colorToDepth_copy, str(obj_id), (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 4, cv2.LINE_AA)
                 cv2.putText(colorToDepth_copy, str(obj_id), (x + 10, y + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
-            colorToDepth_copy = cv2.resize(colorToDepth_copy, (640, 480))
-            frameState.colorToDepthFrameObject = colorToDepth_copy
+            
 
+    colorToDepth_copy = cv2.resize(colorToDepth_copy, (640, 480))
+    frameState.colorToDepthFrameObject = colorToDepth_copy
     box_limits = [c for contour_list in contours for c in contour_list if c.size > 0]
     print("Número Objetos:", len(box_limits))
 
