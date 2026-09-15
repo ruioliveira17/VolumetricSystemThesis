df5274103ae31e0b40c9cd221d0e8615fb7538a3
Rui Oliveira <ruijoliveira2003@gmail.com>
2026-09-15 09:36:00 +0100

    Script v9

--- install.sh
+++ install.sh
@@ -100,10 +100,6 @@ sudo apt install -y chromium
 cat > "$HOME/start_qubic.sh" <<EOF
 #!/bin/bash
 
-until curl -s http://localhost:5173 > /dev/null; do
-    sleep 1
-done
-
 /usr/bin/chromium --kiosk --password-store=basic http://localhost:5173
 EOF
 
@@ -121,6 +117,7 @@ Type=simple
 User=$(id -un)
 WorkingDirectory=$PROJECT_DIR
 ExecStart=$PROJECT_DIR/.venv/bin/python $PROJECT_DIR/run_api.py
+ExecStartPost=/bin/bash -c 'until curl -s http://localhost:5173 > /dev/null; do sleep 1; done'
 Restart=always
 RestartSec=5
 
