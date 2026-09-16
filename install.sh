c05ec1ccbfa97ef26f141f67aede0e7eea2dbb17
Rui Oliveira <ruijoliveira2003@gmail.com>
2026-09-16 16:31:38 +0100

    Changes 14

--- install.sh
+++ install.sh
@@ -127,7 +127,7 @@ done
 /usr/bin/chromium \
     --kiosk \
     --password-store=basic \
-    --default-background-color=ff0a0a0a \
+    --default-background-color=0a0a0a \
     http://localhost:8000 &
 EOF
 
